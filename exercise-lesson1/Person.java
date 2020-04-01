95abd0bb5e9c20b8f49e08bfc6dad30c61126987
magnuen2k <maen030@student.kristiania.no>
2020-04-01 02:42:16 +0200

    pusing test javaclass from lesson1

---
+++ exercise-lesson1/Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    private String navn;
+
+    public void Person(String navn){
+        this.navn = navn;
+    }
+
+    public String showName(){
+        return navn;
+    }
+
+    public void setName(String navn){
+        this.navn = navn;
+    }
+}
